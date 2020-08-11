f6a669fbd7b32aeaa0accf6a0ff1b2b7aac14ec5
infoTercen <info@tercen.com>
2020-08-11 11:54:18 +0000

    added scaling per channel

--- main.R
+++ main.R
@@ -2,10 +2,28 @@ library(tercen)
 library(dplyr)
 
 ctx = tercenCtx()
-scale = as.double(ctx$op.value('scale'))
+scale             = as.double(ctx$op.value('scale'))
+scale_per_channel = as.double(ctx$op.value('scale per channel'))
 
-ctx %>% 
-  select(.y) %>% 
-  transmute(asinh = asinh(.y/scale)) %>%
+if (scale_per_channel){
+  row_df = ctx %>% rselect() %>% mutate(.ri = row_number()-1)
+  scale_name_sym = sym(ctx$rnames[[2]])
+  
+  ctx %>% 
+    select(.ri, .ci, .y) %>% 
+    left_join(row_df, by = ".ri") %>% 
+    group_by(.ri, .ci) %>% 
+    mutate(asinh = asinh(.y)/!!scale_name_sym) %>%
+    select(.ri, .ci, asinh) %>%
+    ctx$addNamespace() %>%
+    ctx$save()
+} else {
+
+  ctx  %>%
+  select(.ri, .ci, .y) %>%
+  group_by(.ri, .ci) %>% 
+  mutate(asinh = asinh(.y)/scale) %>%
+  select(.ri, .ci, asinh) %>%
   ctx$addNamespace() %>%
   ctx$save()
+}
--- workspace.R
+++ workspace.R
@@ -1,21 +1,40 @@
 library(tercen)
 library(dplyr)
 
-options("tercen.serviceUri" = "http://127.0.0.1:5400/api/v1")
-options("tercen.username"= "admin")
-options("tercen.password" = "admin")
+options("tercen.serviceUri"= "https://tercen.com/api/v1/")
+options("tercen.username"= "fnaji")
+options("tercen.password"= "obama")
 
-options("tercen.workflowId"= "1b5b9c21c7cbf0613462606fba00b2f2")
-options("tercen.stepId"= "3-1")
+options("tercen.workflowId"= "7efcf43fe12c5104b13ccbd7f659226e")
+options("tercen.stepId"= "b5abd33b-d136-44e3-9e55-39370ae9b851")
 getOption("tercen.workflowId")
 getOption("tercen.stepId")
 
 
+
 # scale = as.double(ctx$op.value('scale'))
-scale = 5
+# scale = 5
+
+ctx = tercenCtx()
+
+sn = sym(ctx$rnames[[2]])
+
+row_df = ctx %>% rselect() %>% mutate(.ri = row_number()-1)
 
 (ctx = tercenCtx()) %>% 
-  select(.y) %>% 
-  transmute(asinh = asinh(.y)/scale) %>%
+  select(.ri, .ci, .y) %>% 
+  left_join(row_df, by = ".ri") %>% 
+  group_by(.ri, .ci) %>% 
+  mutate(asinh = asinh(.y)/!!sn) %>%
+  select(.ri, .ci, asinh) %>%
+  ctx$addNamespace() %>%
+  ctx$save()
+  
+
+df <- ctx  %>%
+  select(.ri, .ci, .y) %>%
+  group_by(.ri, .ci) %>% 
+  mutate(asinh = asinh(.y)/scale) %>%
+  select(.ri, .ci, asinh) %>%
   ctx$addNamespace() %>%
   ctx$save()
